597522a98ba9f2a409e7e027cdc9b92819b2baa7
ubuntu <xiao-shanmin>
2017-11-24 16:14:41 +0800

    add the test branch

---
+++ my_test/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    unsigned u=10,u2=42;
+    int i=-42;
+    std::cout<<i+i<<std::endl;
+    std::cout<<u+i<<std::endl;
+    std::cout<<u-u2<<std::endl;
+    std::cout<<1e-2<<'\a'<<std::endl;
+    std::cout << "\7Hello,"\
+            " World!" << std::endl;
+    return 0;
+}
